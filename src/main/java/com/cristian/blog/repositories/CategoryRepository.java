6450d2d307b4550c92436e94c388d1a50390f333
CriFigShe <crifigshe@gmail.com>
2025-08-03 12:00:28 +0200

    Category Repository

---
+++ src/main/java/com/cristian/blog/repositories/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.cristian.blog.repositories;
+
+import com.cristian.blog.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+
+
+}
