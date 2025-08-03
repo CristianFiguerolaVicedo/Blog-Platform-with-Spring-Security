9adc7d39da42833f6910383a2b44f9ccd93a9081
CriFigShe <crifigshe@gmail.com>
2025-08-03 12:01:16 +0200

    Tag Repository

---
+++ src/main/java/com/cristian/blog/repositories/TagRepository.java
@@ -0,0 +1,13 @@
+package com.cristian.blog.repositories;
+
+import com.cristian.blog.domain.entities.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+
+
+}
