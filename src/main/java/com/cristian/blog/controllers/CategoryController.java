727ce6bcf7dba13e357890d8afd972f8b45128ef
CriFigShe <crifigshe@gmail.com>
2025-08-03 13:12:10 +0200

    Create Category Endpoint

--- src/main/java/com/cristian/blog/controllers/CategoryController.java
+++ src/main/java/com/cristian/blog/controllers/CategoryController.java
@@ -1,14 +1,16 @@
 package com.cristian.blog.controllers;
 
 import com.cristian.blog.domain.dtos.CategoryDTO;
+import com.cristian.blog.domain.dtos.CreateCategoryRequest;
 import com.cristian.blog.domain.entities.Category;
 import com.cristian.blog.mappers.CategoryMapper;
 import com.cristian.blog.services.CategoryService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +30,17 @@ public class CategoryController {
                 .toList();
 
         return ResponseEntity.ok(categories);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> createCategory(
+            @Valid @RequestBody CreateCategoryRequest createCategoryRequest
+    ) {
+        Category categoryToCreate = categoryMapper.toEntity(createCategoryRequest);
+        Category savedCategory = categoryService.createCategory(categoryToCreate);
+        return new ResponseEntity<>(
+                categoryMapper.toDto(savedCategory),
+                HttpStatus.CREATED
+        );
     }
 }
