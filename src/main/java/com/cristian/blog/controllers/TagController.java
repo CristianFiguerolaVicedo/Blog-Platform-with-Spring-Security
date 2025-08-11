2b28586e5d7e901585dd3815632ea89d80b6f217
CriFigShe <crifigshe@gmail.com>
2025-08-11 13:11:00 +0200

    Create tags endpoint

--- src/main/java/com/cristian/blog/controllers/TagController.java
+++ src/main/java/com/cristian/blog/controllers/TagController.java
@@ -1,14 +1,14 @@
 package com.cristian.blog.controllers;
 
+import com.cristian.blog.domain.dtos.CreateTagsRequest;
 import com.cristian.blog.domain.dtos.TagResponse;
 import com.cristian.blog.domain.entities.Tag;
 import com.cristian.blog.mappers.TagMapper;
 import com.cristian.blog.services.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +25,15 @@ public class TagController {
         List<Tag> tags = tagService.getTags();
         List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
         return ResponseEntity.ok(tagResponses);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> creteTags(@RequestBody CreateTagsRequest createTagsRequest) {
+        List<Tag> savedTags = tagService.createTags(createTagsRequest.getNames());
+        List<TagResponse> createdTagResponses = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        return new ResponseEntity<>(
+                createdTagResponses,
+                HttpStatus.CREATED
+        );
     }
 }
